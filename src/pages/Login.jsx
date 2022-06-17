f1fcee7fdd3502d6531b234b012d79782ae37ca5
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-06-16 22:49:47 -0600

    add Desingning a React App

---
+++ src/pages/Login.jsx
@@ -0,0 +1,45 @@
+import Google from "../img/google.png";
+import Facebook from "../img/facebook.png";
+import Github from "../img/github.png";
+
+
+
+const Login = () =>{
+
+    return(
+        <div className="login">
+            <h1 className="loginTitle">Choose a Login Method</h1>
+            <div className="wrapper">
+                <div className="left">
+                    <div className="loginButton google">
+                        <img src={Google} alt="" className="icon" />
+                        Google
+                    </div>
+
+                    <div className="loginButton facebook">
+                        <img src={Facebook} className="icon" alt="" />
+                        Facebook
+                    </div>
+
+                    <div className="loginButton github">
+                        <img src={Github} className="icon" alt="" />
+                        Github
+                    </div>
+                </div>
+
+                <div className="center">
+                    <div className="line"/>
+                    <div className="or">OR</div>
+                </div>
+
+                <div className="right">
+                    <input type="text" placeholder="Username" />
+                    <input type="text" placeholder="Password" />
+                    <button className="submit"> Login </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login
